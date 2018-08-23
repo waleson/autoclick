115749bf2dc41a9fc5713c124e94627ed00823f0
waleson <cnwei20042005@163.com>
2018-08-23 22:00:04 +0800

    Update common.js

--- common.js
+++ common.js
@@ -1,5 +1,6 @@
  
 var start=false;
+var walesonc=0;
 var gwaleson=document.getElementsByClassName("pro-get-button-box")[0];
 var gClickBtn=document.getElementsByClassName("J_grab_single")[0];
 function StartAuto(){
@@ -17,15 +18,18 @@ function CheckClick()
     timeok=(h>=10&&m>1&&m<=60);
     if(timeok){
       gClickBtn.click();
+      document.getElementById("waleson_auto_click").innerHTML="正在自动抢单"+walesonc+"次";
+      walesonc+=1;
     }    
   }
 }
 
 function AddBtn(){
   br=document.createElement("br")
-	abtn=document.createElement("a");
+  abtn=document.createElement("a");
   abtn.innerHTML="开启自动抢单";
   abtn.setAttribute("class","btn btn-yellow btn-mid");
+  abtn.setAttribute("id","waleson_auto_click");
   abtn.setAttribute("href", "javascript:StartAuto()");
   gwaleson.appendChild(br)
 	gwaleson.appendChild(abtn);	
