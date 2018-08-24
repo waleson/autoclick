e79a67817c35f40afb6533cd6e86b4b769faaf06
waleson <cnwei20042005@163.com>
2018-08-24 15:53:05 +0800

    Update common.js

--- common.js
+++ common.js
@@ -1,20 +1,21 @@
 var walesonc=0;
-var gwaleson=document.getElementsByClassName("pro-get-button-box")[0];
-var gClickBtn=document.getElementsByClassName("J_grab_single")[0];
 
 function StartAuto(){
   start=sessionStorage.getItem("ClickStart");
   if(start=="true"){
-    sessionStorage.setItem("ClickStart", "false");
+    sessionStorage.setItem("ClickStart", "false");    
   }else{
-    sessionStorage.setItem("ClickStart", "true");
+    sessionStorage.setItem("ClickStart", "true");    
   }
   walesonAddBtn=document.getElementById("waleson_auto_click");
-  if(start=="false"){
-     walesonAddBtn.innerHTML="已停止自动抢单";  
-  }else{
-     walesonAddBtn.innerHTML="已开启自动抢单";  
-  };
+  if(walesonAddBtn!=null){
+      if(start=="false"){
+         walesonAddBtn.innerHTML="已停止自动抢单";  
+      }else{
+         walesonAddBtn.innerHTML="已开启自动抢单";  
+      };
+   };
+   window.location.reload();
 }
 
 function CheckClick()
@@ -27,10 +28,20 @@ function CheckClick()
     m=mydate.getMinutes();
     s=mydate.getSeconds();
     timeok=(h>=9&&m>1&&m<=60);
-    if(timeok){ 
-      document.getElementById("waleson_auto_click").innerHTML="正在自动抢单"+walesonc+"次";
-      walesonc+=1;
-      gClickBtn.click();
+    if(timeok){       
+      gClickBtn=document.getElementsByClassName("J_grab_single")[0];
+      if (gClickBtn==null){
+        window.location.reload();
+      }else{        
+        wbtn=document.getElementById("waleson_auto_click");
+        if (wbtn!=null){
+        wbtn.innerHTML="正在自动抢单"+walesonc+"次";
+        walesonc+=1;
+        gClickBtn.click();
+       }else{
+         window.location.reload();
+       }
+      }  
     }    
   }
 }
@@ -42,17 +53,10 @@ function AddBtn(){
   abtn.setAttribute("class","btn btn-yellow btn-mid");
   abtn.setAttribute("id","waleson_auto_click");
   abtn.setAttribute("href", "javascript:StartAuto()");
+  gwaleson=document.getElementsByClassName("pro-get-button-box")[0];
   gwaleson.appendChild(br)
   gwaleson.appendChild(abtn); 
 }
 
-function AutoRefresh(){
-   start=sessionStorage.getItem("ClickStart");
-   if(start=="true"){
-    window.location.reload();
-   };
-}
-
 setInterval("CheckClick()",200);
-setInterval("AutoRefresh()",600);
 AddBtn();
