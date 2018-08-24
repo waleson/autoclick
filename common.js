a253f7292344eb38e32d4a0be17531fbe780ed62
waleson <cnwei20042005@163.com>
2018-08-24 14:34:28 +0800

    Update common.js

--- common.js
+++ common.js
@@ -1,14 +1,16 @@
-
-
-var start=false;
 var walesonc=0;
 var gwaleson=document.getElementsByClassName("pro-get-button-box")[0];
 var gClickBtn=document.getElementsByClassName("J_grab_single")[0];
 var walesonAddBtn=document.getElementById("waleson_auto_click");
 function StartAuto(){
-  start=!start;
+  start=sessionStorage.getItem("ClickStart");
+  if(start=="true"){
+    sessionStorage.setItem("ClickStart", "false");
+  }else{
+    sessionStorage.setItem("ClickStart", "true");
+  }
   walesonAddBtn=document.getElementById("waleson_auto_click");
-  if(!start){
+  if(start=="false"){
      walesonAddBtn.innerHTML="已停止自动抢单";  
   }else{
      walesonAddBtn.innerHTML="已开启自动抢单";  
@@ -17,7 +19,8 @@ function StartAuto(){
 
 function CheckClick()
 {
-  if(start){
+  start=sessionStorage.getItem("ClickStart");
+  if(start=="true"){
     mydate=new Date();
     timeok=false;
     h=mydate.getHours();
@@ -25,7 +28,7 @@ function CheckClick()
     s=mydate.getSeconds();
     timeok=(h>=9&&m>1&&m<=60);
     if(timeok){   
-      document.execCommand('Refresh');
+      window.location.reload();
       walesonAddBtn.innerHTML="正在自动抢单"+walesonc+"次";
       walesonc+=1;
       gClickBtn.click();
@@ -41,7 +44,7 @@ function AddBtn(){
   abtn.setAttribute("id","waleson_auto_click");
   abtn.setAttribute("href", "javascript:StartAuto()");
   gwaleson.appendChild(br)
-	gwaleson.appendChild(abtn);	
+  gwaleson.appendChild(abtn); 
 }
 
 setInterval("CheckClick()",200);
